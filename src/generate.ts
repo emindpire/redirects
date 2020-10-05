7c51da5517679f04b2196cb00580393e30b3bb13
Murtaja Ziad <murtaja.ziad2014@gmail.com>
2020-10-05 20:37:17 +0300

    fix: Refactor files.

---
+++ src/generate.ts
@@ -0,0 +1,39 @@
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+var files: any = [];
+
+async function main() {
+  let dirs = await getDirs();
+
+  await dirs.forEach(async (dir: string) => {
+    let file = await readFileSync(
+      join(__dirname, "..", "redirects", dir, "_redirects"),
+      {
+        encoding: "utf8",
+      }
+    );
+
+    await files.push(dir + file);
+  });
+
+  await write(files.join("\n"));
+}
+
+function getDirs() {
+  return readdirSync(join(__dirname, "..", "redirects")).filter((dir: string) =>
+    dir.endsWith("emindpire.com")
+  );
+}
+
+function write(data: string) {
+  try {
+    writeFileSync(join(__dirname, "..", "_redirects"), data);
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+main();
